cef482413334e22d7e30d51ae7935ce8ea1aacc4
visaplmokn <ostaz.soliman.206@gmail.com>
2026-06-24 14:51:21 +0200

    Update device admin messages to Arabic

--- app/src/main/java/com/example/workprofiledpc/MyDeviceAdminReceiver.kt
+++ app/src/main/java/com/example/workprofiledpc/MyDeviceAdminReceiver.kt
@@ -1,4 +1,4 @@
-package com.example.workprofiledpc
+package com.example.dpc
 
 import android.app.admin.DeviceAdminReceiver
 import android.content.Context
@@ -6,19 +6,13 @@ import android.content.Intent
 import android.widget.Toast
 
 class MyDeviceAdminReceiver : DeviceAdminReceiver() {
-
     override fun onEnabled(context: Context, intent: Intent) {
         super.onEnabled(context, intent)
-        Toast.makeText(context, "DPC Device Administration Enabled", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "تم تفعيل صلاحيات مدير الجهاز بنجاح!", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDisabled(context: Context, intent: Intent) {
         super.onDisabled(context, intent)
-        Toast.makeText(context, "DPC Device Administration Disabled", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onProfileProvisioningComplete(context: Context, intent: Intent) {
-        super.onProfileProvisioningComplete(context, intent)
-        Toast.makeText(context, "Work Profile Provisioning Completed Successfully!", Toast.LENGTH_LONG).show()
+        Toast.makeText(context, "تم إلغاء صلاحيات مدير الجهاز.", Toast.LENGTH_SHORT).show()
     }
 }
